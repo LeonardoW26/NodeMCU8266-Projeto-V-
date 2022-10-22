563440f07eb2d45c8a8b811d855479f59445f1f9
LeonardoW26 <leonardoww26@gmail.com>
2022-10-21 21:28:32 -0300

    Commit 21/10/2022

--- NodeMCU8266-Projeto-V-/src/main.cpp
+++ NodeMCU8266-Projeto-V-/src/main.cpp
@@ -3,6 +3,10 @@
 #define buzzer 5
 #define pinPIR2 15
 
+bool porta = false;
+bool mesa = false;
+
+
 void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(pinPIR, INPUT);
@@ -16,18 +20,33 @@ void loop() {
      digitalWrite(buzzer, LOW);
   } else {
      Serial.println("DETECTADO");
-     digitalWrite(buzzer, HIGH);
-     tone(buzzer,50);
+     porta = true;
   }
 
   bool valorPIR2 = digitalRead(pinPIR2);
   if (valorPIR2) {
      Serial.println("-------");
-     digitalWrite(buzzer, LOW);
+     
   } else {
      Serial.println("DETECTADO");
-     digitalWrite(buzzer, HIGH);
-     tone(buzzer,50);
+     mesa = true;
   }
+
+
+   if(valorPIR2 == true){
+      mesa == false;
+   }
+
+   if (valorPIR == true && mesa == false){
+      porta == false;
+      Serial.println(porta);
+   }
+   
+   if(valorPIR == true && mesa == true){
+      digitalWrite(buzzer, LOW);
+      Serial.println(porta);
+   }else{
+      digitalWrite(buzzer, HIGH);
+   }
 
 }
