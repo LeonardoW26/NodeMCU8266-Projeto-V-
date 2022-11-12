8dfa4b7696e63339724b9734890f1c9741cec737
LeonardoW26 <leonardoww26@gmail.com>
2022-11-11 22:12:38 -0300

    11/11/2022 deu certo!!!!!!!!!!!!!!!!!!

--- NodeMCU8266-Projeto-V-/src/main.cpp
+++ NodeMCU8266-Projeto-V-/src/main.cpp
@@ -6,6 +6,7 @@
 bool porta = false;
 bool mesa = false;
 bool controle = false;
+bool controle2 = false;
 
 void setup() {
   pinMode(buzzer, OUTPUT);
@@ -15,7 +16,7 @@ void setup() {
 }
  
 void loop() {
-
+   if(controle2 == false){
       if (porta == false){
          bool valorPIR = digitalRead(pinPIR);
          Serial.println("Sensor porta");
@@ -35,14 +36,49 @@ void loop() {
             delay(1000);
             digitalWrite(buzzer, LOW);
             mesa = true;
+            controle = true;
+            controle2 = true;
          }
+
+      }
+   }
+      if(controle == true){
+         Serial.print("CONTROLE");
+         bool valorPIR = digitalRead(pinPIR);
+         bool valorPIR2 = digitalRead(pinPIR2);
+            if(valorPIR2 == true && valorPIR == false){
+            digitalWrite(buzzer, HIGH);
+            delay(5000);
+            digitalWrite(buzzer, LOW);
+
+            controle = false;
+            mesa =false;
+            porta = false;
+            controle2 = false;
+            }
+            if(valorPIR2 == false){
+               digitalWrite(buzzer, HIGH);
+               delay(200);
+               digitalWrite(buzzer, LOW);
+               delay(200);
+               digitalWrite(buzzer, HIGH);
+               delay(200);
+               digitalWrite(buzzer, LOW);
+               
+               controle = false;
+               mesa =false;
+               porta = false;
+               controle2 = false;
+            }
+            }
+         
+
       }
 
       
 
 
-
-      if (porta == true){
+/*if (porta == true){
          bool valorPIR = digitalRead(pinPIR);
          Serial.println("Sensor mesa");
       Serial.println(valorPIR);
@@ -62,8 +98,7 @@ void loop() {
             digitalWrite(buzzer, LOW);
             porta = false;
          }
-      }
+      }*/
 
-      }
 
 
