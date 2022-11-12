93d5dfdfa520bbfa03f623e28baf78e437919a94
LeonardoW26 <leonardoww26@gmail.com>
2022-11-11 21:18:24 -0300

    codigo 11/11/2022

--- NodeMCU8266-Projeto-V-/src/main.cpp
+++ NodeMCU8266-Projeto-V-/src/main.cpp
@@ -1,50 +1,69 @@
 #include <Arduino.h>
 #define pinPIR 13
 #define buzzer 5
-#define pinPIR2 15
+#define pinPIR2 4
 
 bool porta = false;
 bool mesa = false;
-
+bool controle = false;
 
 void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(pinPIR, INPUT);
+  pinMode(pinPIR2, INPUT);
   Serial.begin(9600);
 }
  
 void loop() {
-  bool valorPIR = digitalRead(pinPIR);
-  if (valorPIR) {
-     Serial.println("-------");
-     digitalWrite(buzzer, LOW);
-  } else {
-     Serial.println("DETECTADO");
-     porta = true;
-  }
-  
-   bool valorPIR2 = digitalRead(pinPIR2);
-   if(valorPIR2 == true){
-      mesa = false;
-      if (valorPIR2) {
-         Serial.println("-------");
-     
-  } else {
-     Serial.println("DETECTADO");
-     mesa = true;
-  }
-   }
 
-   if (valorPIR == true && mesa == false){
-      porta = false;
-      Serial.println(porta);
-   }
-   
-   if(valorPIR == true && mesa == true){
-      digitalWrite(buzzer, LOW);
-      Serial.println(porta);
-   }else{
-      digitalWrite(buzzer, HIGH);
-   }
+      if (porta == false){
+         bool valorPIR = digitalRead(pinPIR);
+         Serial.println("Sensor porta");
+      Serial.println(valorPIR);
+         if(valorPIR == false){
+            digitalWrite(buzzer, HIGH);
+            delay(1000);
+            digitalWrite(buzzer, LOW);
+            porta = true;
+         }
+      }
+      if(porta == true){
+         Serial.println("sensor mesa");
+         bool valorPIR2 = digitalRead(pinPIR2);
+         if(valorPIR2 == false){
+            digitalWrite(buzzer, HIGH);
+            delay(1000);
+            digitalWrite(buzzer, LOW);
+            mesa = true;
+         }
+      }
 
-}
+      
+
+
+
+      if (porta == true){
+         bool valorPIR = digitalRead(pinPIR);
+         Serial.println("Sensor mesa");
+      Serial.println(valorPIR);
+         if(valorPIR == false){
+            digitalWrite(buzzer, HIGH);
+            delay(1000);
+            digitalWrite(buzzer, LOW);
+            mesa = false;
+         }
+      }
+      if(porta == false){
+         Serial.println("sensor porta");
+         bool valorPIR2 = digitalRead(pinPIR2);
+         if(valorPIR2 == false){
+            digitalWrite(buzzer, HIGH);
+            delay(1000);
+            digitalWrite(buzzer, LOW);
+            porta = false;
+         }
+      }
+
+      }
+
+
